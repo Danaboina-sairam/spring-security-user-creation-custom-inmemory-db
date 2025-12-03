41aed49d35bc1b09f0c9bcba5cd0679c9b550de9
Sairam Danaboina <sairam8276@gmail.com>
2025-12-03 19:12:46 +0530

    spring security user creation custom inmemory db

--- src/main/java/com/spring/security/user/creation/custom/inmemory/db/SecurityConfig.java
+++ src/main/java/com/spring/security/user/creation/custom/inmemory/db/SecurityConfig.java
@@ -1,17 +1,24 @@
 package com.spring.security.user.creation.custom.inmemory.db;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user1= User.withUsername("my_username_1")
                 .password("{noop}my_password_1")
